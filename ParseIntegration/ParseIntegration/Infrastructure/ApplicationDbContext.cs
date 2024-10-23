8f582ff45ed77f9367766d83036b5897759bd4b5
dan1yal7 <daniiltemirkhanov@gmail.com>
2024-10-24 03:20:14 +0500

    point3

---
+++ ParseIntegration/ParseIntegration/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParseIntegration.Models;
+
+namespace ParseIntegration.Infrastructure
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
---
+++ ParseIntegration/ParseIntegration/Models/Employees.cs
@@ -0,0 +1,17 @@
+﻿namespace ParseIntegration.Models
+{
+    public class Employees
+    {
+        public string Payroll_Number { get; set; }
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+        public DateTime Date_of_Birth { get; set; }
+        public string Telephone { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string Address_2 { get; set; }
+        public string Postcode { get; set; }
+        public string Email_Home { get; set; }
+        public DateTime Start_Date { get; set; }
+    }
+}
